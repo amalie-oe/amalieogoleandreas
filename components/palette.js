e963c45cc57c29840c5b68c131b3b6fdb1ed2262
amalie-oe <amalieoe@hotmail.com>
2026-08-24 20:43:46 +0200

    Update palette.js

--- components/palette.js
+++ components/palette.js
@@ -1,8 +1,8 @@
 export const palette = {
-  mist: "#EAF2F8", //ICE BLUE
-  linen: "#F2EFE7", //BEIGE
-  stone: "#98B4C7", //AIRY BLUE
-  slate: "#3D5A80", //STEEL BLUE
-  steel: "#3D5A80", //STEEL BLUE
-  navy: "#1B263B",//NAVY
+  mist: "#D8D8D6",
+  linen: "#CBC4B9",
+  stone: "#ADA69B",
+  slate: "#67717E",
+  steel: "#45505F",
+  navy: "#38414F",
 };
